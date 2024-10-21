7915525d4b4003d8afa66f769eac5cfb290fd5a7
dianbayunugroho <bayu@newtronic-solution.com>
2024-10-21 13:34:07 +0700

    future func: 21 oct 2024

--- bin/future_dart/future.dart
+++ bin/future_dart/future.dart
@@ -1,21 +1,15 @@
 void main() {
-  //Ini Future Completed with data, untuk memanggil return darimethod getOrder() menggunakan .then()
-  // getOrder().then((value) {
-  //   print("Orderan kamu : $value");
-  // });
-
   //Ini Future Completed with error, untuk memanggil error nya menggunakan method catchError()
-  getOrder().then((value) {
+  getOrder(false).then((value) {
     print("Orderan kamu : $value");
   }).catchError((error) {
     print("Errornya $error");
   });
 }
 
-Future<String> getOrder() {
+Future<String> getOrder(bool isStockAvailable) {
   //Ini Future Uncompleted karena belum dipangil di method manapun
   return Future.delayed(Duration(seconds: 1), () {
-    var isStockAvailable = false;
     if (isStockAvailable) {
       return 'Coffee Boba';
     } else {
